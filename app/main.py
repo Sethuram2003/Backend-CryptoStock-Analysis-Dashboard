3caf8fcbda7d055b1a18ed6b3e2f820bfc18df38
Sethuram2003 <gauthamr2003@gmail.com>
2025-11-27 11:30:15 -0500

    finishing the endpoint integration with the workflow

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-
+from app.api.routes.crypto.get_current_sentiment import crypto_sentiment_router
 from app.api.routes.crypto.get_crypto_data import crypto_data_router
 from app.api.routes.crypto.get_crypto_historical import crypto_historical_router
 from app.api.routes.health_check import health_check_router
@@ -9,4 +9,5 @@ app = FastAPI(title="Crypto and Stock Analysis Service", version="1.0.0")
 
 app.include_router(health_check_router)
 app.include_router(crypto_data_router)
+app.include_router(crypto_sentiment_router)
 app.include_router(crypto_historical_router)
