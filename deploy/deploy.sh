ad813a2c9a7a011ddfb41cd5e3e5bab49c5e5ee6
AdebayoBraimah <adebayo.braimah@gmail.com>
2025-12-02 15:32:10 -0500

    BUG: Stock/Crypto Data ingestion consistently fails

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
 cwd=$(pwd)
+scripts_dir=$(dirname $(realpath "${0}"))
+parent_dir=$(dirname "${scripts_dir}")
 
-cd ../
+# echo "${parent_dir}"
+
+cd "${parent_dir}"
 
 # Deploy to Hetzner VPS
 rsync -avz --exclude '.git' --exclude 'venv' --exclude '__pycache__' ./ root@178.156.209.160:/opt/app
 
-cd "$
+cd "${cwd}"
 
 # Launch:
+#  cd /opt/app
 #  docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
 # 
 # Check if running (on VPS):
@@ -17,3 +22,8 @@ cd "$
 # 
 # View on (local) web browser:
 # http://178.156.209.160:8080
+# 
+# Docker stop containers (on VPS):
+# docker compose down 
+# # OR
+# docker stop $(docker ps -q)
